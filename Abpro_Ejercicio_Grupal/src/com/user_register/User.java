fc88ca4e6aed29f5fab7ca3890e7bb39fdbc1bc5
Jose Mora <j.alfonso.m.castillo7@gmail.com>
2023-01-16 18:30:39 -0300

    To do por hacer

---
+++ Abpro_Ejercicio_Grupal/src/com/user_register/User.java
@@ -0,0 +1,33 @@
+package com.user_register;
+
+public class User {
+
+	public static void main(String[] args) {
+		
+		// TODO tres perfiles: cliente, profesional y administrativo. 
+		
+		// TODO crear: nombre, fecha de nacimiento y RUN. 
+		
+		//TODO   registrar: Cliente: dirección, teléfono, cantidad de empleados
+		
+		
+		Profesional: años de experiencia, departamento
+		Administrativo: función, nombre superior
+		Se pide un algoritmo y pieza de código Java que solicite una cantidad de usuarios a ingresar, y que
+		pida por cada uno de ellos los campos que son comunes y el tipo a registrar. Dependiendo del tipo,
+		debe pedir los campos se piden según perfil; una vez ingresados los campos, se deben mostrar por
+		pantalla.
+		El algoritmo concluye una vez que se ha ingresado la cantidad de usuarios indicada inicialmente.
+		
+		
+		
+		
+	   //caracteristicas -> atributes
+		String nombre = "";
+		
+		// acciones -> metodos
+		
+	}
+	public static void
+
+}
