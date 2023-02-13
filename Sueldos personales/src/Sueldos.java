34d515810be342d34d92c17fc51780b26f5585b0
Jose Mora <j.alfonso.m.castillo7@gmail.com>
2023-02-13 20:49:34 -0300

    Arreglado

--- Sueldos personales/src/Sueldos.java
+++ Sueldos personales/src/Sueldos.java
@@ -1,6 +1,8 @@
 
 public class Sueldos {
 	
+	public static void main(String[] args) {
+        // TODO Auto-generated method stub
 	    protected final int salarioMinimo = 410000;
 	    protected int sueldo; 
 
@@ -9,7 +11,7 @@ public class Sueldos {
 	            System.out.println("La persona recibe salario mínimo");
 	        } else if (sueldo < salarioMinimo) {
 	            System.out.println("La persona esta pobre");
-	        } else if (sueldo > salarioMinimo) {
+	        } else  (sueldo > salarioMinimo) {
 	            System.out.println("La persona esta bien económica");
 	        }
 	    }
@@ -19,14 +21,12 @@ public class Sueldos {
 	            System.out.println("Recibe Bono Marzo");
 	        } else if (sueldo < salarioMinimo) {
 	            System.out.println("Recibe Bono Marzo más canasta familiar");
-	        } else if (sueldo > salarioMinimo) {
+	        } else (sueldo > salarioMinimo) {
 	            System.out.println("La persona esta bien económica no recibe nada");
 	        }
 	    }
 	    
 	    
-	    
-	    public static void main(String[] args) {
-	        // TODO Auto-generated method stub
-	    }
+	   
+	  
 	}
